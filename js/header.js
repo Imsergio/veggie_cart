c7a8f861ffd6ff1c1ed8f7e519bafeb670f9564f
imsergio <uhmarquez7@gmail.com>
2025-11-23 16:32:14 -0500

    feat: add initial html structure
    Create the base HTML structure including header with navigation, main content area, and footer. Includes meta tags, favicon, font imports, and links to CSS/JS files.

---
+++ js/header.js
@@ -0,0 +1,30 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('navMenu'),
+      navToggle = document.getElementById('menuToggle'),
+      navClose = document.getElementById('navClose');
+
+/*===== MENU SHOW =====*/
+/* Valida si la constante existe */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Valida si la constante existe */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+/* Funcionalidad para cerrar el menú móvil cuando se hace clic en un enlace */
+const navLinks = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    // Al hacer clic en cada nav__link, se elimina la clase show-menu
+    navMenu.classList.remove('show-menu');
+}
+navLinks.forEach(n => n.addEventListener('click', linkAction));
